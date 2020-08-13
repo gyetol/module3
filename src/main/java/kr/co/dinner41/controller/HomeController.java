3ad1a76fb5b22c381e16747aa88ca5e3a3e44273
KIMJUSUNG <dlrtls12345@naver.com>
2020-08-13 19:26:34 +0900

    make package

--- src/main/java/kr/co/dinner41/controller/HomeController.java
+++ src/main/java/kr/co/dinner41/controller/HomeController.java
@@ -1,4 +1,4 @@
-package kr.co.dinner41.Controller;
+package kr.co.dinner41.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ src/main/java/kr/co/dinner41/interceptor/Interceptor.java
@@ -0,0 +1,6 @@
+package kr.co.dinner41.interceptor;
+
+public interface Interceptor {
+	
+	// 인터셉터 다시 만드세요!!!
+}
