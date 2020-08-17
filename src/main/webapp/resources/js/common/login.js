b58453893f43100d8031ceaae022637842785626
ahjheeju129 <heeju1112@naver.com>
2020-08-17 23:57:26 +0900

    ISSUE-25 logout 버튼 연결

--- src/main/webapp/resources/js/common/login.js
+++ src/main/webapp/resources/js/common/login.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
-	alert("${errorMessage}");
 	var contextPath=sessionStorage.getItem("contextPath");
-	console.log(contextPath);
+	var errorMessage=$('#errorMessage').val();
+	if(errorMessage){
+		alert(errorMessage);
+	}
+	//console.log(contextPath);
 	$('#register_button').on("click",function(){
-		alert("회원가입버튼에 클릭이벤트 발생");
+		//alert("회원가입버튼에 클릭이벤트 발생");
 		location.href=contextPath+"/register";
 	});
 	$('#password_button').on("click",function(){
-		alert("passwordButton의 onClick이벤트 발생");
+		//alert("passwordButton의 onClick이벤트 발생");
 		location.href=contextPath+"/password";
 	});
 
