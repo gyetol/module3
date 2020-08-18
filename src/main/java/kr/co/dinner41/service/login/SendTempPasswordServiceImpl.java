6b58ce6b0326545f8dba3b9e8dea0a05515ae549
moosongsong <50647845+moosongsong@users.noreply.github.com>
2020-08-18 16:02:10 +0900

    update Manager

--- src/main/java/kr/co/dinner41/service/login/SendTempPasswordServiceImpl.java
+++ src/main/java/kr/co/dinner41/service/login/SendTempPasswordServiceImpl.java
@@ -20,48 +20,48 @@ public class SendTempPasswordServiceImpl implements SendTempPasswordService {
 
 	@Override
 	public void execute(String to) throws LoginException {
-//		final String from="heeju1999@gmail.com";
-//		String host="smtp.gmail.com";
-//
-//	    Properties prop = System.getProperties();
-//
-//        prop.put("mail.smtp.host", "smtp.gmail.com");
-//        //prop.put("mail.smtp.port", "587");
-//        prop.put("mail.smtp.port", "465");
-//        prop.put("mail.smtp.auth", "true");
-//        prop.put("mail.stmp.ssl.enable", "true");
-//
-////        Authenticator auth = new Authenticator() {
-////        	@Override
-////        	protected PasswordAuthentication getPasswordAuthentication() {
-////        		return new PasswordAuthentication(from, password)
-////        	}
-////
-////		};
-//
-////        Session session = Session.getDefaultInstance(prop, auth);
-//
-////        MimeMessage msg = new MimeMessage(session);
-//
-//        try {
-//            msg.setSentDate(new Date());
-//
-//            msg.setFrom(new InternetAddress("shxorld@gmail.com", "VISITOR"));
-////            InternetAddress to = new InternetAddress("szhyun2002@gmail.com");
-//            msg.setRecipient(Message.RecipientType.TO, to);
-//            msg.setSubject("제목", "UTF-8");
-//            msg.setText("안녕하세요 테스트 메일입니다.", "UTF-8");
-//
-//            Transport.send(msg);
-//
-//        } catch(AddressException ae) {
-//            System.out.println("AddressException : " + ae.getMessage());
-//        } catch(MessagingException me) {
-//            System.out.println("MessagingException : " + me.getMessage());
-//        } catch(UnsupportedEncodingException e) {
-//            System.out.println("UnsupportedEncodingException : " + e.getMessage());
-//        }
-//
+		final String from="heeju1999@gmail.com";
+		String host="smtp.gmail.com";
+
+	    Properties prop = System.getProperties();
+
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        //prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.port", "465");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.stmp.ssl.enable", "true");
+
+        Authenticator auth = new Authenticator() {
+        	@Override
+        	protected PasswordAuthentication getPasswordAuthentication() {
+        		return new PasswordAuthentication(from, password);
+        	}
+
+		};
+
+        Session session = Session.getDefaultInstance(prop, auth);
+
+        MimeMessage msg = new MimeMessage(session);
+
+        try {
+            msg.setSentDate(new Date());
+
+            msg.setFrom(new InternetAddress("shxorld@gmail.com", "VISITOR"));
+            InternetAddress to = new InternetAddress("szhyun2002@gmail.com");
+            msg.setRecipient(Message.RecipientType.TO, to);
+            msg.setSubject("제목", "UTF-8");
+            msg.setText("안녕하세요 테스트 메일입니다.", "UTF-8");
+
+            Transport.send(msg);
+
+        } catch(AddressException ae) {
+            System.out.println("AddressException : " + ae.getMessage());
+        } catch(MessagingException me) {
+            System.out.println("MessagingException : " + me.getMessage());
+        } catch(UnsupportedEncodingException e) {
+            System.out.println("UnsupportedEncodingException : " + e.getMessage());
+        }
+
 	}
 
 }
