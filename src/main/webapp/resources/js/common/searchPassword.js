e0328039d0fdbb00f3e2058802a142e7c4f7f6c0
ahjheeju129 <heeju1112@naver.com>
2020-08-18 16:12:41 +0900

    {ISSUE-27} 임시 비밀번호 발급 완료

--- src/main/webapp/resources/js/common/searchPassword.js
+++ src/main/webapp/resources/js/common/searchPassword.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
+	var errorCode=$('#modelData').data("errorcode");
+	var errorMessage=$('#modelData').data("errormessage");
+	if (errorCode){
+		alert(errorMessage+"");
+		if(errorCode==="1"){
+			console.log('err코드가 0 입니다');
+			$('#user_email').focus();
+		}
+		else if(errorCode==="2"){
+			let defaultEmail=$('#modelData').data("defaultemail");
+			$('#user_email').val(defaultEmail);
+			$('#password_button').focus();
+		}
+	}
+
 	$('#password_button').on("click",function(){
 		//alert('임시비밀번호 받기 버튼 클릭 이벤트 발생');
 		let email=$('#user_email').val();
